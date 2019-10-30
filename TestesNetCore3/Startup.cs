6dad73d02d06feec025d13bb5d53d0dc9f9979bd
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2019-10-29 23:37:14 -0300

    Instalando e configurando pacote para Health Check da API

--- TestesNetCore3/Startup.cs
+++ TestesNetCore3/Startup.cs
@@ -1,15 +1,21 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mime;
 using System.Threading.Tasks;
+using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace TestesNetCore3
 {
@@ -26,6 +32,15 @@ namespace TestesNetCore3
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            ConfigureHealthChecks(services);
+        }
+
+        private void ConfigureHealthChecks(IServiceCollection services)
+        {
+            services.AddHealthChecks()
+                .AddSqlServer(Configuration.GetConnectionString("LOCAL"), name: "AdventureWorks2017");
+            services.AddHealthChecksUI();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,6 +61,37 @@ namespace TestesNetCore3
             {
                 endpoints.MapControllers();
             });
+
+            app.UseHealthChecks("/status",
+               new HealthCheckOptions()
+               {
+                   ResponseWriter = async (context, report) =>
+                   {
+                       var result = JsonConvert.SerializeObject(
+                           new
+                           {
+                               statusApplication = report.Status.ToString(),
+                               healthChecks = report.Entries.Select(e => new
+                               {
+                                   check = e.Key,
+                                   ErrorMessage = e.Value.Exception?.Message,
+                                   status = Enum.GetName(typeof(HealthStatus), e.Value.Status)
+                               })
+                           });
+                       context.Response.ContentType = MediaTypeNames.Application.Json;
+                       await context.Response.WriteAsync(result);
+                   }
+               });
+
+            // Gera o endpoint que retornará os dados utilizados no dashboard
+            app.UseHealthChecks("/healthchecks-data-ui", new HealthCheckOptions()
+            {
+                Predicate = _ => true,
+                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+            });
+
+            // Ativa o dashboard para a visualização da situação de cada Health Check
+            app.UseHealthChecksUI();
         }
     }
 }
